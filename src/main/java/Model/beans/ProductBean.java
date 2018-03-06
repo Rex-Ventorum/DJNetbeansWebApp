8e82179596692f37456d8ad5545966eb1b425b3f
Programming <Programming@Crypta-Sophiae>
2018-03-05 20:37:38 -0600

    Displaying Product List Works

--- src/main/java/Model/beans/ProductBean.java
+++ src/main/java/Model/beans/ProductBean.java
@@ -9,12 +9,15 @@ import javax.inject.Named;
 import javax.enterprise.context.SessionScoped;
 import java.io.Serializable;
 import java.util.List;
+import javax.faces.context.FacesContext;
+import javax.faces.event.ActionEvent;
+import javax.servlet.ServletContext;
 
 
 @Named(value = "productBean")
 @SessionScoped
 public class ProductBean implements Serializable {
-
+    
     private ProductService productService;
     private Product product;
     private String listHeader;
@@ -25,7 +28,8 @@ public class ProductBean implements Serializable {
     ////////////////////////////////////////
     
     public ProductBean() {
-        String path = "WEB-INF" + File.separatorChar + "WebProducts.txt";  
+       ServletContext servletContext = (ServletContext) FacesContext.getCurrentInstance().getExternalContext().getContext();
+        String path = servletContext.getRealPath("WEB-INF" + File.separatorChar + "WebProducts.txt");  
         File file = new File(path);
         try {
             productService = new FileReaderProductService(file);
@@ -34,6 +38,7 @@ public class ProductBean implements Serializable {
         }
         if(productService != null){
             productList = productService.getAllProducts();
+            listHeader = "All Products";
         }
     }
     
@@ -41,31 +46,37 @@ public class ProductBean implements Serializable {
     // ------ INTERACTABLE METHODS ------ //
     ////////////////////////////////////////
     
-    public final String displayAllProducts(){
+    public String displayAllProducts(){
         productList = productService.getAllProducts();
         listHeader = "All Products";
         return "product-list.xhtml";
+    }
+    
+    public void displayProductSearch(){
+        String searchParam = (String) event.getComponent().getAttributes().get("searchParam");
+        productList = productService.searchProducts(searchParam);
+        listHeader = "Search Results For: " + searchParam;
     }
     
     ////////////////////////////////////////
     // --------- SETTER METHODS --------- //
     ////////////////////////////////////////
     
-    public final void setProductService(ProductService productService) throws IllegalArgumentException {
+    public void setProductService(ProductService productService) throws IllegalArgumentException {
         if (productService == null) {
             throw new IllegalArgumentException("productService May Not Be Null");
         }
         this.productService = productService;
     }
 
-    public final void setProduct(Product product) throws IllegalArgumentException {
+    public void setProduct(Product product) throws IllegalArgumentException {
         if (product == null) {
             throw new IllegalArgumentException("product May Not Be Null");
         }
         this.product = product;
     }
 
-    public final void setProductList(List<Product> productList) throws IllegalArgumentException {
+    public void setProductList(List<Product> productList) throws IllegalArgumentException {
         if (productList == null) {
             throw new IllegalArgumentException("productList May Not Be Null");
         }
@@ -76,19 +87,19 @@ public class ProductBean implements Serializable {
     // --------- GETTER METHODS --------- //
     ////////////////////////////////////////
 
-    public final ProductService getProductService() {
+    public ProductService getProductService() {
         return productService;
     }
 
-    public final Product getProduct() {
+    public Product getProduct() {
         return product;
     }
 
-    public final List<Product> getProductList() {
+    public List<Product> getProductList() {
         return productList;
     }   
 
-    public final String getListHeader() {
+    public String getListHeader() {
         return listHeader;
     }
 }
