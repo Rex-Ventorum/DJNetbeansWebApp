63d96d84428a236416e6ff15a8a1b4b8d1e3fdb6
Programming <Programming@Crypta-Sophiae>
2018-03-09 15:09:58 -0600

    Vary Basic Adding To Cart from Product Description Page Functonalilty Implemented

--- src/main/java/Model/beans/ProductBean.java
+++ src/main/java/Model/beans/ProductBean.java
@@ -9,9 +9,7 @@ import javax.inject.Named;
 import javax.enterprise.context.SessionScoped;
 import java.io.Serializable;
 import java.util.List;
-import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
-import javax.faces.event.AjaxBehaviorEvent;
 import javax.servlet.ServletContext;
 
 @Named(value = "productBean")
---
+++ src/main/java/Model/beans/ShoppingCartBean.java
@@ -0,0 +1,86 @@
+package Model.beans;
+
+import Model.Pojos.Product;
+import Model.Pojos.ShoppingCart;
+import javax.inject.Named;
+import javax.enterprise.context.SessionScoped;
+import java.io.Serializable;
+import javax.faces.bean.ManagedProperty;
+import javax.swing.JOptionPane;
+
+@Named(value = "cartBean")
+@SessionScoped
+public class ShoppingCartBean implements Serializable {
+   
+    private int qnt;
+    private ShoppingCart shoppingCart;
+    
+    ////////////////////////////////////////
+    // ---------- CONSTRUCTORS ---------- //
+    ////////////////////////////////////////
+    
+    public ShoppingCartBean() {
+        shoppingCart = new ShoppingCart();
+    }
+    
+    ////////////////////////////////////////
+    // ------ INTERACTABLE METHODS ------ //
+    ////////////////////////////////////////
+    
+    public String displayShoppingCart(){
+        return "shopping-cart.xhtml";
+    }
+    
+    public String addToCart(Product product){
+        shoppingCart.editCartItem(product, qnt);
+        return "shopping-cart.xhtml";
+    }
+    
+    ////////////////////////////////////////
+    // --------- SETTER METHODS --------- //
+    ////////////////////////////////////////
+    
+    public void setShoppingCart(ShoppingCart shoppingCart) throws IllegalArgumentException {
+        if (shoppingCart == null) {
+            throw new IllegalArgumentException("shoppingCart May Not Be Null");
+        }
+        this.shoppingCart = shoppingCart;
+    }
+
+    public void setQnt(int qnt) throws IllegalArgumentException {
+        if (qnt < 0) {
+            throw new IllegalArgumentException("qnt May Not Be Negative");
+        }
+        this.qnt = qnt;
+    }
+        
+    ////////////////////////////////////////
+    // --------- GETTER METHODS --------- //
+    ////////////////////////////////////////
+    
+    public ShoppingCart getShoppingCart() {
+        return shoppingCart;
+    }
+
+    public int getQnt() {
+        return qnt;
+    }
+    
+    ////////////////////////////////////////
+    // ----- PRIVATE HELPER METHODS ----- //
+    ////////////////////////////////////////
+    
+    ////////////////////////////////////////
+    // --------- OTHER METHOID ---------- //
+    ////////////////////////////////////////
+
+    
+
+    
+
+    
+
+    
+
+    
+}
